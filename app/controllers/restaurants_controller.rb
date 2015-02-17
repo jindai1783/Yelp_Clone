5937b1631730c430423d57a13dfb7e0069c66742
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-17 18:01:46 +0000

    finisihing the form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,18 @@
+# require '../helpers/restaurant_helper'
+
 class RestaurantsController < ApplicationController
+  
+  include RestaurantsHelper
+
   def index
     @restaurants = Restaurant.all
   end
 
   def new
-    
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
   end
 end
