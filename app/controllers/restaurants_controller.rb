81e17b5c1829a6db11f0ceb18db9316b23438fcd
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-17 18:58:56 +0000

    adidng an update of a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,4 +22,15 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     puts @restaurant
   end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
 end
