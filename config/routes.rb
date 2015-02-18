4ec30281fd0bc1b1dcb7b03b8a4c88255b9be420
Jin Dai <jindai1783@gmail.com>
2015-02-18 13:51:55 +0000

    Added Devise utilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
 
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
