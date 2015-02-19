a16520a9c303dcba919818e16545b9b95bf2bfb8
Jin Dai <jindai1783@gmail.com>
2015-02-19 17:29:46 +0000

    Added test for two stars

--- spec/helpers/review_helper_spec.rb
+++ spec/helpers/review_helper_spec.rb
@@ -5,5 +5,14 @@ describe ReviewsHelper, :type => :helper do
     it 'does nothing for not a number' do
       expect(helper.star_rating('N/A')).to eq 'N/A'
     end
+
+    it 'returns five black stars for five' do
+      expect(helper.star_rating(5)).to eq '★★★★★'
+    end
+
+    it 'returns three black stars for three' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
   end
 end
