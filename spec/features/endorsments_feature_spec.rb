68639017358e2551721708ab5aa4320fb99c8bc2
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-19 19:01:28 +0000

    some problems

--- spec/features/endorsments_feature_spec.rb
+++ spec/features/endorsments_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'endorsing reviews' do
 
   scenario 'a user can endorse a review,which updates the review ednorsment count' do 
     visit '/restaurants'
-    click_link 'Endorse KFC'
+    click_link 'Endorse Review'
     expect(page).to have_content('1 endorsement')
   end
 
