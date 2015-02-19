b0d6ccdc5584487cd21268686565e55d845b13e3
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-19 12:30:22 +0000

    reviews for logged users only with a test

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,7 +4,18 @@ feature 'reviewing' do
 
   before {Restaurant.create name: 'KFC'}
 
+  scenario 'user cannot leave a review if not logged in' do 
+    visit '/restaurants'
+    expect(page).not_to have_link('Review')
+  end
+
   scenario 'allows user to leave a review using a form' do 
+    visit '/'
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
     visit '/restaurants'
     click_link 'Review KFC'
     fill_in "Thoughts", with: "so so"
