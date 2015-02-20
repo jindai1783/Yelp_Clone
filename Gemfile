1279bbdfe1589eb496ece2d3c9390cb7d765e83d
Jin Dai <jindai1783@gmail.com>
2015-02-20 12:16:18 +0000

    Added test for javascript style endorsement

--- Gemfile
+++ Gemfile
@@ -48,4 +48,8 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'rspec-collection_matchers'
+
+  gem 'poltergeist'
+  gem 'database_cleaner'
+
 end
--- spec/features/endorsments_feature_spec.rb
+++ spec/features/endorsments_feature_spec.rb
@@ -10,7 +10,13 @@ feature 'endorsing reviews' do
   scenario 'a user can endorse a review,which updates the review ednorsment count' do 
     visit '/restaurants'
     click_link 'Endorse Review'
-    expect(page).to have_content('1 endorsement')
+    expect(page).to have_content('1 endorsements')
   end
+
+    it 'a user can endorse a review, which increments in the endorsement count', js: true do
+      visit '/restaurants'
+      click_link 'Endorse Review'
+      expect(page).to have_content("1 endorsements")
+    end
 
 end
